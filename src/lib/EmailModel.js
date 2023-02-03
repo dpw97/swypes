36cb2efead98bd88c668b8def74db7c06f44584b
dpw97 <dweaver1017@gmail.com>
2023-02-02 21:20:23 -0800

    Deleted mongoose model

--- src/lib/EmailModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const { Schema, model } = mongoose;
-
-const EmailSchema = new Schema({
-  email: String,
-});
-const EmailModel = model('EmailModel', EmailSchema);
-exports.EmailModel = EmailModel;
