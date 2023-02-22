16449ec742a317a4350586b74c0602b25a5e7cf5
dpw97 <dweaver1017@gmail.com>
2023-02-22 00:17:50 -0800

    remove unused endpoints

--- src/lib/router.js
+++ src/lib/router.js
@@ -47,56 +47,5 @@ router.post('/signup', (req, res) => {
       }
     });
 });
-// POST endpoint to add a new restaurant
-router.post('/restaurants', (req, res) => {
-  const db = GetDB.getDb('RestaurantDB');
-  const restaurant = {
-    name: req.body.name,
-    phone: req.body.phone,
-    website: req.body.website,
-    image: req.body.image,
-  };
-  db
-    .collection('Restaurants')
-    .insertOne(restaurant, (err) => {
-      if (err) {
-        res.status(400).send('error inserting restaurant');
-      } else {
-        res.status(200).send('success inserting restaurant');
-      }
-    });
-});
-
-// PUT endpoint to update a specific restaurant by name
-router.put('/restaurants/:name', (req, res) => {
-  const db = GetDB.getDb('RestaurantDB');
-  const { name } = req.params;
-  const restaurant = {
-    name: req.body.name,
-    phone: req.body.phone,
-    website: req.body.website,
-    image: req.body.image,
-  };
-  db.collection('Restaurants').updateOne({ name }, { $set: restaurant }, (err, result) => {
-    if (err) {
-      res.status(404).send('The restaurant with the given name was not found.');
-    } else {
-      res.json(result);
-    }
-  });
-});
-
-// DELETE endpoint to delete a specific restaurant by id
-router.delete('/restaurants/:id', (req, res) => {
-  const db = GetDB.getDb('RestaurantDB');
-  const { id } = req.params;
-  db.collection('Restaurants').deleteOne({ _id: id }, (err, result) => {
-    if (err) {
-      res.status(404).send('The restaurant with the given name was not found.');
-    } else {
-      res.status(200).json(result);
-    }
-  });
-});
 
 module.exports = router;
