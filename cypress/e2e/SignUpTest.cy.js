35161c9bfca2102c317e07dd17ea405883961217
dpw97 <dweaver1017@gmail.com>
2023-02-22 12:49:05 -0800

    Added signup test

--- cypress/e2e/SignUpTest.cy.js
+++ cypress/e2e/SignUpTest.cy.js
@@ -1,8 +1,24 @@
 /* eslint-disable no-undef */
-describe('Sign Up API testing', () => {
-  it('Adds email to database - POST', () => {
+describe('SignUp', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:5173/');
-    cy.get('input').type('test@email.com');
-    cy.get('input').should('have.value', 'test@email.com');
+  });
+
+  it('displays the email input', () => {
+    cy.get('input[name="email"]').should('be.visible');
+  });
+
+  it('validates the email input', () => {
+    cy.get('input[name="email"]').type('invalid-email');
+    cy.contains('Enter').click();
+    cy.contains('Enter a valid Email').should('be.visible');
+  });
+
+  it('submits the form and shows a success message', () => {
+    cy.intercept('/api/signup').as('signup');
+    cy.get('input[name="email"]').type('test@example.com');
+    cy.contains('Enter').click();
+    cy.wait('@signup').its('response.statusCode').should('eq', 200);
+    cy.contains('Thank you for signing up!').should('be.visible');
   });
 });
