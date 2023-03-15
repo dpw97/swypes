1a2ea58ba3f6f8d56b6394d923e7a0592d41d767
dpw97 <dweaver1017@gmail.com>
2023-03-14 20:47:12 -0700

    remove unused styles

--- src/app/routes/Home.jsx
+++ src/app/routes/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from '../components/Hero';
 import HowItWorks from '../components/HowItWorks';
 import Week from '../components/Week';
-import '../styles/Home.css';
 
 function Home() {
   return (
