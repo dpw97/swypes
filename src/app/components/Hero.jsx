843ef49c3a7f689d41e2c0f4826e89e2c5a27c97
dpw97 <dweaver1017@gmail.com>
2023-03-20 19:54:55 -0700

    Change hero styling

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,18 +1,29 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import Logo from '../../../assets/hero.jpg';
 import '../styles/Hero.css';
-import SignUp from './SignUp';
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
+import Logo from '../../../assets/hero.jpg';
 
 function Hero() {
   return (
-    <div className="hero" style={{ backgroundImage: `url(${Logo})` }}>
+    <div
+      className="hero"
+      style={{
+        backgroundImage:
+        `linear-gradient(
+        rgba(0, 0, 0, 0.5),
+        rgba(0, 0, 0, 0.5)
+        ),
+        url(${Logo})`,
+      }}
+    >
       <div className="hero-content-container">
         <div className="hero-content">
-          <h1>Swypes</h1>
           <h2>Your one-stop shop for college meal plans</h2>
+          <h3>Let us handle your meals so you can focus on your education</h3>
           <p>Sign up today to be notified when you can start picking up your meals </p>
-          <div className="signup"><SignUp /></div>
+          <Link to="contact"><Button type="primary">Get Started</Button></Link>
         </div>
       </div>
     </div>
