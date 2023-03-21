0bbc6dcd144974911767da8a986d132b20d345b4
dpw97 <dweaver1017@gmail.com>
2023-03-20 20:36:37 -0700

    change style

--- src/app/components/HowItWorks.jsx
+++ src/app/components/HowItWorks.jsx
@@ -3,28 +3,25 @@ import '../styles/HowItWorks.css';
 
 function HowItWorks() {
   return (
-    <div className="container">
+    <div className="step-container">
       <h2>How it works</h2>
-      <div className="step-container">
-        <div className="step1">
+      <div className="steps">
+        <div className="step">
           <h3>Step 1</h3>
           <p>Sign up for our meal plan service by providing your email address.</p>
         </div>
-        <div className="step2">
+        <div className="step">
           <h3>Step 2</h3>
           <p>Select your meal plan and customize it to your dietary preferences and schedule.</p>
         </div>
-        <div className="step3">
+        <div className="step">
           <h3>Step 3</h3>
           <p>Pick up your meals from our designated location at your college campus.</p>
         </div>
-        <div className="step4">
+        <div className="step">
           <h3>Step 4</h3>
           <p>Enjoy delicious and nutritious meals that will fuel your academic success.</p>
         </div>
-      </div>
-      <div className="image-container">
-        <img src="" alt="" />
       </div>
     </div>
   );
